3d81f93dceabde5c45a6f2ff2dee39f3b584a4ff
Aathira <23041002@myrp.edu.sg>
2024-11-28 13:42:17 +0800

    hi

--- src/components/PackingList.js
+++ src/components/PackingList.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import Item from './Item';
  
-function PackingList({ items, handleDeleteItem, handleUpdateItem }) {
+function PackingList({ items, onDelete, onUpdate }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item
-            key={item.id}
-            item={item}
-            handleDeleteItem={handleDeleteItem}
-            handleUpdateItem={handleUpdateItem}
-          />
+          <Item key={item.id} item={item} onDelete={onDelete} onUpdate={onUpdate} />
         ))}
       </ul>
     </div>
